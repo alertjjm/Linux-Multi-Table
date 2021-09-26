3816fc40275684cbc54968e6cfef6ae8c7b2c738
alertjjm <sdr2936@gmail.com>
2021-09-26 13:56:41 +0900

    Feat: input validation

--- multi-table.sh
+++ multi-table.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+if [ $# -ne 2 ]
+then
+    echo "Usage: ./multi-table num1 num2"
+    exit 1
+elif [ $1 -lt 0 ] || [ $2 -lt 0 ] || [ $1 -gt 9 ] || [ $2 -gt 9 ]
+then
+    echo "Error: The arguments must be integers in range of 1 ~ 9"
+    exit 1
+fi
 for i in $(seq 1 $1)
 do
     for j in $(seq 1 $2)
